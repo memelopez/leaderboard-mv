3833c13d7a4fb0745ed9c93f8322ad08bb5a7d10
memelopez <meme30lopez@gmail.com>
2022-02-03 23:41:04 -0600

    Add imports and hardcoded scores

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 // /src/index.js
 import './styles.css';
+import { renderListZone, renderList } from './modules/leaderboardList';
+import renderFormZone from './modules/leaderboardForm';
 
-const displayStuff = () => { };
+// Hardcoded scores
+const scoreList = [];
+const score1 = {};
+score1.name = 'John';
+score1.points = 100;
+scoreList.push(score1);
+const score2 = {};
+score2.name = 'Jane';
+score2.points = 333;
+scoreList.push(score2);
+const score3 = {};
+score3.name = 'Joe';
+score3.points = 7;
+scoreList.push(score3);
+const score4 = {};
+score4.name = 'Jenny';
+score4.points = 55;
+scoreList.push(score4);
+
+const displayStuff = () => {
+  renderListZone();
+  renderList(scoreList);
+  renderFormZone();
+};
 
 // When content loads
 document.addEventListener('DOMContentLoaded', displayStuff());
